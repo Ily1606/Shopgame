5bf0d297bbcbd0c64b6df6847d615b5348418afa
HuyMagic-12 <72398417+HuyMagic-12@users.noreply.github.com>
2020-10-26 23:43:11 +0700

    HuyMagic

--- index.php
+++ index.php
@@ -3,30 +3,35 @@
 <head>
 <meta charset="utf-8">
 <title>Shopgame</title>
-<link rel="stylesheet" href="css3/index.css"> 
+<link rel="stylesheet" href="/css3/index.css"> 
+<link rel="stylesheet" href="/assets/fontawesome/css/all.css">
 </head>
 <body>
-<div class="main">
+<div class="header-main">
 <div class="header">
-<div class="contact-information">s
-<div class="connect">
+<div class="contact-information">
+    <div class="connect">
     <span>Kênh người bán</span>
-    <span>|</span>
-    <span>Kết nối</span>
+    <span class="header-space">|</span>
+    <span class="header-space">Kết nối</span>
+    <span class="social-network">
+    <i class="fab fa-facebook"></i>
+    <i class="fab fa-google-plus-g"></i>
+    </span>
 </div>
-<div class="information-user"></div>
+<div class="information-user">
+     <i class="fas fa-bell"></i>
+     <span>Thông báo</span>
+     <span class="user-space">|</span>
+     <i class="fas fa-question-circle"></i>
+     <span class="user-space">Trợ giúp</span>
+    <span class="user-space">|</span>
+    <span class="user-space">Đăng nhập</span>
+    <span class="user-space">|</span>
+    <span class="user-space">Đăng kí</span>
 </div>
-    huy
-    huymagic
-    
-
-
 </div>
-
-
-
-
 </div>
-
+</div>
 </body>
 </html>
