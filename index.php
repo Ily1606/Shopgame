d011b28bfcb22e1b2a0dcd1b7c7626a586132113
HuyMagic-12 <72398417+HuyMagic-12@users.noreply.github.com>
2020-10-26 21:28:23 +0700

    HuyMagic

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
-<?php
+<DOCTYPE html></DOCTYPE>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Shopgame</title>
+<link rel="stylesheet" href="css3/index.css"> 
+</head>
+<body>
+<div class="main">
+<div class="header">
+<div class="contact-information">
+<div class="connect">
+    <span>Kênh người bán</span>
+    <span>|</span>
+    <span>Kết nối</span>
+</div>
+<div class="information-user"></div>
+</div>
 
-echo "Test";
-?>
+
+</div>
+
+
+
+
+</div>
+
+</body>
+</html>
