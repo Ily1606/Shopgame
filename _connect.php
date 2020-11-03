51508cb12a6be1aeb5b5eeffe9226bb6b07935ce
Nguyên Nguyên <no1.ily1606@gmail.com>
2020-11-03 14:17:47 +0700

    Upadte

--- _connect.php
+++ _connect.php
@@ -3,8 +3,6 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "shopgame";
-
-<<<<<<< HEAD
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -12,7 +10,3 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
-=======
-
-?>kk
->>>>>>> b1306eb06fd57197d04e5bead12c6d50e9fa0709
